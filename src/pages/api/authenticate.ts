9623adc2f050c96f3222d2f3c308026b40f9f84e
Mohammed <mohammed.ammourie@gmail.com>
2024-10-12 02:13:58 +0300

    er

--- src/pages/api/authenticate.ts
+++ src/pages/api/authenticate.ts
@@ -83,7 +83,7 @@ export default async function handler(
         .json({ errors: [{ field: "password", error: "Incorrect password" }] });
     }
   } catch (error) {
-    console.error("Error finding user:", error);
+    // console.error("Error finding user:", error);
     return res.status(500).json({
       errors: [
         {
