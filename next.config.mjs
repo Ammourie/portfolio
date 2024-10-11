a7031b31bfbf27fca8d45aec0c583ddd5c071818
Mohammed <59035839+Ammourie@users.noreply.github.com>
2024-10-12 00:32:32 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,7 @@ const nextConfig = {
     async headers() {
         return [
             {
-                source: '/(.*)',
+                source: '/api/:path*',
                 headers: [
                     {
                         key: 'Access-Control-Allow-Origin',
