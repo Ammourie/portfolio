fddd8c53bce53e69c2aab7c7c16d8ac5c3f004f2
Mohammed <mohammed.ammourie@gmail.com>
2024-10-12 01:23:04 +0300

    refactor

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,23 @@ import mdx from '@next/mdx';
 
 const withMDX = mdx({
     extension: /\.mdx?$/,
-    options: { },
+    options: {},
 });
+
+const corsHeaders = [
+    {
+        key: 'Access-Control-Allow-Origin',
+        value: '*',
+    },
+    {
+        key: 'Access-Control-Allow-Methods',
+        value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+    },
+    {
+        key: 'Access-Control-Allow-Headers',
+        value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+    },
+];
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -12,20 +27,7 @@ const nextConfig = {
         return [
             {
                 source: '/api/authenticate',
-                headers: [
-                    {
-                        key: 'Access-Control-Allow-Origin',
-                        value: '*',
-                    },
-                    {
-                        key: 'Access-Control-Allow-Methods',
-                        value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-                    },
-                    {
-                        key: 'Access-Control-Allow-Headers',
-                        value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-                    },
-                ],
+                headers: corsHeaders,
             },
         ];
     },
